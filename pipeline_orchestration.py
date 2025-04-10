92f14a934eaa54f01c96f2ef66980f69bb1b0205
tech.dgarcia <tech.dgarcia@gmail.com>
2025-04-10 10:37:59 -0700

    Update pipeline_orchestration.py

--- pipeline_orchestration.py
+++ pipeline_orchestration.py
@@ -1,1 +1,16 @@
+import boto3
+
+def start_pipeline(execution_name="ml-retraining-exec"):
+    client = boto3.client("stepfunctions")
+
+    response = client.start_execution(
+        stateMachineArn="arn:aws:states:REGION:ACCOUNT_ID:stateMachine:MLRetrainingPipeline",
+        name=execution_name,
+        input='{"trigger": "scheduled_retrain"}'
+    )
+
+    print("Pipeline execution started:", response["executionArn"])
+
+if __name__ == "__main__":
+    start_pipeline()
 
